27aef8d22fccb8a4f64ea3fa443cd4dbd9694853
Medea <logmedha09@gmail.com>
2023-05-21 15:49:40 +0100

    Create style.js

---
+++ style.js
@@ -0,0 +1,35 @@
+body {
+  background: blue;
+  color: white;
+  text-align: center;
+  font-family: "Comic Sans MS", cursive, sans-serif;
+}
+.button {
+  background-color: yellow;
+  color: yellow;
+  border: 2px solid yellow;
+  position: absolute;
+  font-size: 30px;
+}
+
+.button:hover {
+  background-color: yellow;
+  color: #ff0000;
+  font-size: 30px
+}
+
+#myCanvas {
+  background: yellow;
+  border: 1px solid black;
+  position: relative;
+  width: 1250px;
+  height: 400px;
+}
+
+body{
+  user-select: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+}
